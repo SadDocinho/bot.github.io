95e09f1c489e4a147362a0959627b0856afe9c05
Felipe Monteiro <67602498+SadDocinho@users.noreply.github.com>
2022-12-16 18:19:55 -0300

    UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
-import { initializeApp } from 'firebase/app';
-import { collection, getDocs, getFirestore } from 'firebase/firestore';
-import { useState } from 'react';
-
-const firebaseapp =  initializeApp({
-  apiKey: "AIzaSyAdxx-mHEoVYNs7vVb_X84X26V6Ux_OIzY",
-  authDomain: "candyland-6ecc8.firebaseapp.com",
-  projectId: "candyland-6ecc8",
-});
-
 export const App = () => {
   return <h1>CandyLand</h1>;
 };
